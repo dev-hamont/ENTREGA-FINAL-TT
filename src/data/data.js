a4f53d95653edfa54f9f2aa1a0ad54e23f90d619
dev-hamont <haroldmont777@gmail.com>
2025-12-14 23:24:26 -0300

    Entrega Final

---
+++ src/data/data.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: "entrega-final-tt",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: "613969741496",
+    appId: process.env.FIREBASE_APP_ID
+};
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+  // Initialize Firestore
+const db = getFirestore(app);
+
+export { db };
